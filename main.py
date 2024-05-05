097d12ce0a53a241a3b5d46ade7015fb1e244434
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-06 00:01:50 +0530

    src: drawing line

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from mazor import Graphics
 
 def main():
     win = Graphics.Window(800,600)
+    l = Graphics.Line(Graphics.Point(50,50),Graphics.Point(400,400))
+    win.draw_line(l,"black")
     win.wait_for_close()
 
 if __name__ == '__main__':
--- mazor/Graphics.py
+++ mazor/Graphics.py
@@ -10,7 +10,7 @@ class Window:
         # set tht title property of the root widget 
         self.__root.title("maze-solver")
         # canvas widget and set to data member 
-        self.__canvas = Canvas(self.__root,heigh=self.height,width=self.width)
+        self.__canvas = Canvas(self.__root,height=self.height,width=self.width)
         self.__canvas.pack()
         self.__is_running = False 
         self.__root.protocol("WM_DELETE_WINDOW",self.close)
@@ -28,3 +28,26 @@ class Window:
 
     def close(self):
         self.__is_running = False 
+
+    def draw_line(self,line,fill_color):
+        line.draw(self.__canvas,fill_color)
+    
+    def close(self):
+        self.__is_running = False 
+
+class Point:
+    def __init__(self,x,y):
+        # x cooridante(horizontal) in pixels of the point 
+        # y coordiante(vertical) in pixels of the point 
+        self.x = x
+        self.y = y 
+
+    
+class Line:
+    def __init__(self,point1:Point,point2:Point):
+        self.point1 = point1 
+        self.point2 = point2
+    
+    def draw(self,canvas: Canvas,fill_color):
+        canvas.create_line(self.point1.x,self.point1.y,self.point2.x,self.point2.y,fill=fill_color,width=2)
+
