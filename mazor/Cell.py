3111f8149df41642b5c5a5fb09bca9eb88e2d6f8
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-06 00:43:26 +0530

    render red line

--- mazor/Cell.py
+++ mazor/Cell.py
@@ -33,6 +33,22 @@ class Cell:
             line = Line(Point(x1,y2),Point(x2,y2))
             self._win.draw_line(line)
 
+    def draw_move(self,to_cell,undo=False):
+        len1 = abs(self._x2 - self._x1) // 2
+        x_center = len1 + self._x1 
+        y_center = len1 + self._y1 
+
+        len2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = len2 + to_cell._x1
+        y_center2 = len2 + to_cell._y1 
+
+        fill_order = "red"
+        if undo:
+            fill_order = "gray"
+        
+        line = Line(Point(x_center,y_center),Point(x_center2,y_center2))
+        self._win.draw_line(line,fill_order)
+        
 
 
 
